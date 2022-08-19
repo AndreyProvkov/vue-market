60d3d2b589d2d8e8f866ffbdf61e17cb0f0c3dce
Andrey Provkov <provkovasu15@gmail.com>
2022-08-20 03:25:07 +0500

    Change header markup and styles

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,23 +1,25 @@
 <template>
     <header class="header">
         <div class="wrapper">
-            <router-link to='/'>
-                <div class="logo">
-                    <div class="logo__picture">
-                        <img
-                            class="logo__img"
-                            src="@/assets/logo.svg"
-                            alt="logo"
-                        />
+            <div class="flex">
+                <router-link to='/'>
+                    <div class="logo">
+                        <div class="logo__picture">
+                            <img
+                                class="logo__img"
+                                src="@/assets/logo.svg"
+                                alt="logo"
+                            />
+                        </div>
+                        <p class="logo__text">
+                            <span class="uppercase">ФЛАГ</span>&nbsp;Покупки
+                        </p>
                     </div>
-                    <p class="logo__text">
-                        <span class="uppercase">ФЛАГ</span>&nbsp;Покупки
-                    </p>
-                </div>
-            </router-link>
-            <a class="btn header__btn">
-                Регистрация
-            </a>
+                </router-link>
+                <a class="btn">
+                    Регистрация
+                </a>
+            </div>
         </div> 
     </header>
 </template>
@@ -32,17 +34,19 @@ export default {
 %filter-drop-shadow {
     filter: drop-shadow(0 .4rem .2rem rgba(0, 0, 0, .25));
 }
-
+.flex {
+    display: flex;
+    justify-content: space-between;
+    grid-column: 1 / 13;
+}
 a {
     text-decoration: none;
 }
 .header {
     background-color: #FFF;
     box-shadow: .2rem .4rem .8rem rgba(0, 0, 0, .1);
-    padding: 1.7rem 11.6rem;
-    &__btn {
-        grid-column: -3 / -1;
-    }
+    padding: 1.7rem 0;
+    margin-bottom: 2.4rem;
 }
 .logo {
     display: flex;
